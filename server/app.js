3f612a88b7f17ea5ea1f6748e7db8b435dc4414d
kenzan8000 <kenzan8000@gmail.com>
2015-11-28 23:49:49 -0800

    change architecture using socket.io

--- server/app.js
+++ server/app.js
@@ -1,72 +1,15 @@
-/* ************************************************** *
- *                    express                         *
- * ************************************************** */
+var server = require('http').createServer();
+var io = require('socket.io')(server);
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+io.on('connection', function(socket) {
 
-var routes = require('./routes/index');
+  socket.on('event', function(data) {
+      console.log(data);
+  });
 
-var app = express();
-var sassMiddleware = require('node-sass-middleware');
-var path = require('path');
-app.use(sassMiddleware({
-    src: __dirname + '/scss',
-    dest: path.join(__dirname, 'public'),
-    debug: true,
-    outputStyle: 'compressed',
-    prefix:  '/prefix'
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+  socket.on('disconnect', function() {
+  });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
+server.listen(3000);
